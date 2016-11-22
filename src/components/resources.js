e8f2a2bf87aa506490022eb34676367e7e798ed6
Swelly <Surfswell17@gmail.com>
2016-11-22 12:37:42 -0500

    implementing Auth HOC

---
+++ src/components/resources.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default () => {
+	return (
+		<div>
+			<h3>Super Secret Tax Docs</h3>
+			<ul>
+				<li>Idk</li>
+				<li>This</li>
+				<li>Guy</li>
+				<li>Didn't</li>
+				<li>Pay</li>
+				<p>-D.Trump</p>
+			</ul>
+		</div>
+	);
+};
